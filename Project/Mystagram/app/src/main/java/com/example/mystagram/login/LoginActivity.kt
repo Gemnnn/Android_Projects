96f1c6f4541ccd3cb737ed51eacea4cba2d67529
Branden Min <brandenmin@gmail.com>
2023-12-06 05:22:02 -0500

    refactor: MVC to MVVM structure

---
+++ Project/Mystagram/app/src/main/java/com/example/mystagram/login/LoginActivity.kt
@@ -0,0 +1,173 @@
+package com.example.mystagram.login
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.mystagram.MainActivity
+import com.example.mystagram.R
+import com.example.mystagram.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+    val loginViewModel: LoginViewModel by viewModels()
+    private lateinit var auth : FirebaseAuth
+//    private lateinit var googleSignInClient : GoogleSignInClient
+
+//    lateinit var btn_login: Button
+//    lateinit var et_email: EditText
+//    lateinit var et_password: EditText
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+//        loginViewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+        binding.viewModel = loginViewModel
+        binding.activity = this
+        binding.lifecycleOwner = this
+        setObserve()
+//        auth = FirebaseAuth.getInstance()
+//
+//        btn_login = findViewById(R.id.btn_login)
+//        et_email = findViewById(R.id.et_email)
+//        et_password = findViewById(R.id.et_password)
+//
+//        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//            .requestIdToken(getString(R.string.default_web_client_id))
+//            .requestEmail()
+//            .build()
+//
+//        googleSignInClient = GoogleSignIn.getClient(this , gso)
+//
+//        // login
+//        btn_login.setOnClickListener {
+//            signinAndSignup()
+//        }
+//
+//        // Google login
+//        findViewById<Button>(R.id.btn_google_login).setOnClickListener {
+//            signInGoogle()
+//        }
+    }
+    fun setObserve(){
+        loginViewModel.showInputNumberActivity.observe(this){
+            if (it){
+                finish()
+                startActivity(Intent(this, InputNumberActivity::class.java))
+            }
+        }
+
+        loginViewModel.showFindIdActivity.observe(this){
+            if (it){
+                startActivity(Intent(this, FindIdActivity::class.java))
+            }
+        }
+    }
+    fun loginEmail() {
+        loginViewModel.showInputNumberActivity.value = true
+    }
+
+    fun findId(){
+        loginViewModel.showFindIdActivity.value = true
+
+    }
+
+//    fun signinAndSignup(){
+//        auth?.createUserWithEmailAndPassword(et_email.text.toString(), et_password.text.toString())
+//            ?.addOnCompleteListener{
+//                    task ->
+//                if(task.isSuccessful){
+//                    // Createing a user account
+//                    moveMainPage(task.result.user)
+//                }
+//                else if(!task.exception?.message.isNullOrEmpty()){
+//                    // Catch an error
+//                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+//                }
+//                else{
+//                    // Login, if the user has an account
+//                    signinEmail()
+//                }
+//            }
+//    }
+//
+//    fun signinEmail(){
+//        auth?.signInWithEmailAndPassword(et_email.text.toString(), et_password.text.toString())
+//            ?.addOnCompleteListener{
+//                    task ->
+//                if(task.isSuccessful){
+//                    // Login
+//                    moveMainPage(task.result.user)
+//                }
+//                else{
+//                    // Show an error
+//                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+//
+//                }
+//            }
+//    }
+//
+//    fun moveMainPage(user: FirebaseUser?){
+//        if(user != null){
+//            startActivity(Intent(this, MainActivity::class.java))
+//        }
+//    }
+//
+//    // Google Login
+//
+//    private fun signInGoogle(){
+//        val signInIntent = googleSignInClient.signInIntent
+//        launcher.launch(signInIntent)
+//    }
+//
+//    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+//            result ->
+//        if (result.resultCode == Activity.RESULT_OK){
+//
+//            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+//            handleResults(task)
+//        }
+//    }
+//
+//    private fun handleResults(task: Task<GoogleSignInAccount>) {
+//        if (task.isSuccessful){
+//            val account : GoogleSignInAccount? = task.result
+//            if (account != null){
+//                updateUI(account)
+//            }
+//        }else{
+//            Toast.makeText(this, task.exception.toString() , Toast.LENGTH_SHORT).show()
+//        }
+//    }
+//
+//    private fun updateUI(account: GoogleSignInAccount) {
+//        val credential = GoogleAuthProvider.getCredential(account.idToken , null)
+//        auth.signInWithCredential(credential).addOnCompleteListener {
+//            if (it.isSuccessful){
+//                val intent : Intent = Intent(this , MainActivity::class.java)
+//                startActivity(intent)
+//            }else{
+//                Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
+//
+//            }
+//        }
+//    }
+}
